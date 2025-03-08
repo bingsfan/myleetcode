536f1fd7137dce15af84910cd7d04efaddba825a
bingsfan <mh.sun3@outlook.com>
2025-03-08 16:54:46 +0800

    [25/03/08下午，dp子序列带注释]

--- 392.判断子序列.cpp
+++ 392.判断子序列.cpp
@@ -7,6 +7,13 @@
 // @lc code=start
 class Solution {
 public:
+/* 
+    dp[i][j]:s的前i个元素和t的前j个元素，相同子序列的长度
+    如果相等：直接用上了
+    如果不相等，因为s小而t大，缩小t去找s
+    要求判断s是不是t的子序列，s是更小的集合
+    因此只要dp[s.size][t.size]==s.size就是
+*/
     bool isSubsequence(string s, string t) {
         int n1 = s.size();
         int n2 = t.size();
